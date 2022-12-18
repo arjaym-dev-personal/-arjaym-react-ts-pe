0379b8cbb2e94efccac9c60c03509391a7ee9afd
arjay <arjay@nxt.work>
2022-12-18 19:24:22 +0800

    Initial vite react-ts eslint prettier project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "components/layout/Header";
+function App() {
+	return (
+		<div className="App">
+			<h1>
+				Bare bone vite react typescript eslint prettier project setup
+			</h1>
+			<Header />
+		</div>
+	);
+}
+
+export default App;
